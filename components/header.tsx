b527dd320a61799c26b1f678c3cfcf8fe6960168
Katelyn <katelyn.thomas94@gmail.com>
2025-09-12 21:10:31 -0400

    Created header

---
+++ components/header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header style={{
+            border: '1px solid white', padding: '10px', textAlign: 'center'}}>
+            <h1>Welcome to my introductory React App</h1>
+        </header>
+    );
+};
+
+export default Header;
